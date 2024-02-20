ca0ca6d5c113c13563aaabbc20b0759ed34482d0
DenMaz7 <91177753+DenMaz7@users.noreply.github.com>
2024-02-20 19:29:47 +0200

    Finished the instruction in the console and the view of index.html

--- Laboratory_work_1/js/script.js
+++ Laboratory_work_1/js/script.js
@@ -1,13 +1,42 @@
 function triangle(value1 = 0, type1 = "", value2 = 0, type2 = ""){
-    
+        
+    if(type1 === "" || type2 === ""){
+        console.log("Введіть правильну кількість параметрів трикутника та їх значень.");
+        return "failed";
+    }
+
+    if (typeof value1 != 'number' || typeof value2 != 'number') {
+        console.log("Значення параметрів трикутника повинні бути задані числом");
+        return "failed";
+    } 
+
     const validTypes = ["leg", "hypotenuse", "adjacent angle", "opposite angle", "angle"];
     if (!validTypes.includes(type1) || !validTypes.includes(type2)) {
-        console.log("Будь ласка, перечитайте інструкцію та введіть правильні типи елементів.");
+        console.log("Будь ласка, перечитайте інструкцію та введіть правильні параметри трикутника.");
         return "failed";
     }
 
     if(type1 === type2 && type1 != "leg"){
-        console.log("Ви ввели несумісну пару типів елементів.");
+        console.log("Ви ввели несумісну пару параметрів трикутника.");
+        return "failed";
+    }
+
+    if((type1 === "angle" && type2 != "hypotenuse")||(type2 === "angle" && type1 != "hypotenuse")){
+        console.log("Ви ввели несумісну пару параметрів трикутника. Якщо кут — один з параметрів, то іншим обовязково повинна бути гіпотенуза.");
+        return "failed";
+    }
+
+    if((type1 === "opposite angle" && type2 === "adjacent angle")||(type2 === "opposite angle" && type1 === "adjacent angle")){
+        console.log("Ви ввели несумісну пару параметрів трикутника.");
+        return "failed";
+    }
+
+    if((type1 === "adjacent angle" && type2 === "hypotenuse")||
+    (type2 === "adjacent angle" && type1 === "hypotenuse")||
+    (type1 === "opposite angle" && type2 === "hypotenuse")||
+    (type2 === "opposite angle" && type1 === "hypotenuse"))
+    {
+        console.log("Ви ввели несумісну пару параметрів трикутника.");
         return "failed";
     }
 
@@ -18,7 +47,7 @@ function triangle(value1 = 0, type1 = "", value2 = 0, type2 = ""){
 
     let hypotenuse, leg, otherLeg, alpha, beta;
     
-    if (type1 === "leg" && type2 === "leg") {
+    if (type1 === "leg" && type2 === "leg"){
         leg = Math.min(value1, value2);
         otherLeg = Math.max(value1, value2);
         hypotenuse = Math.sqrt(leg * leg + otherLeg * otherLeg);
@@ -45,7 +74,6 @@ function triangle(value1 = 0, type1 = "", value2 = 0, type2 = ""){
             hypotenuse = leg / Math.sin(beta * (Math.PI / 180));
             otherLeg = Math.sqrt(hypotenuse * hypotenuse - leg * leg);
             alpha = 90 - beta;
-            // beta = angle;
         }
         if (type1 === "adjacent angle" || type2 === "adjacent angle"){
             leg = (type1 === "leg") ? value1 : value2;
@@ -53,7 +81,6 @@ function triangle(value1 = 0, type1 = "", value2 = 0, type2 = ""){
             otherLeg = leg * Math.tan(alpha * (Math.PI / 180));
             hypotenuse = leg / Math.cos(alpha * (Math.PI / 180));
             beta = 90 - alpha;
-            // alpha = angle;
         }
     }
     else if ((type1 === "hypotenuse" && type2 === "angle") || (type1 === "angle" && type2 === "hypotenuse")) {
@@ -62,27 +89,43 @@ function triangle(value1 = 0, type1 = "", value2 = 0, type2 = ""){
         leg = hypotenuse * Math.cos(alpha * (Math.PI / 180));
         otherLeg = hypotenuse * Math.sin(alpha * (Math.PI / 180));
         beta = 90 - alpha;
-        // alpha = angle;
-    }
-    // else {
-    //     console.log("Ви вказали некоректні типи, перечитайте, будь ласка, інструкцію.");
-    //     return "failed";
-    // }      
+    }     
 
-    if(beta >= 90 || alpha >= 90){
+    if(beta > 90 || alpha > 90){
         console.log("У прямокутному трикутнику не може бути тупого кута.");
         return "failed";
     }
     
+    if(beta == 90 || alpha == 90){
+        console.log("У прямокутному трикутнику не може бути двох прямих кутів.");
+        return "failed";
+    }
+
     console.log(`a = ${leg}\nb = ${otherLeg}\nc = ${hypotenuse}\nalpha = ${alpha}°\nbeta = ${beta}°`);
     return "success";
 
 }
 
-triangle(13, "leg", 35, "hypotenuse");
-triangle(13, "leg", 25, "opposite angle");
-triangle(18, "leg", 21, "adjacent angle");
-// triangle(21, "adjacent angle", 18, "leg");
-triangle(5, "hypotenuse", 0, "angle");
-triangle(121, "angle", 13, "hypotenuse");
-triangle(84, "leg", 14, "leg");
+console.log("ІНСТРУКЦІЯ");
+console.log("Дана програма дозволяє розв'язати прямокутний трикутник, тобто обчислити всі його невідомі сторони та кути.");
+console.log("Для цього Вам потрібно написати команду triangle(значення1, параметр1, значення2, параметр2), де 'параметри' — це те, що Вам відомо у трикутнику, а  'значання' — числове представлення значень відомих Вам параметрів.");
+console.log("Для обчислення трикутника достатньо мати три параметри. Оскільки Ви обчислюєте прямокутний трикутник, то один з кутів вже відомий — 90°.");
+console.log("Відповідно два інших параметри Ви повинні вказати в дужках при виклику команди triangle.");
+console.log("Ось позначення параметрів та пояснення до них:\n\t• leg — катет\n\t• hypotenuse — гіпотенуза\n\t• adjacent angle — прилеглий до катета кут\n\t• opposite angle — протилежний до катета кут\n\t• angle — один з двох гострих кутів (коли задана гіпотенуза)");
+console.log("");
+console.log('Приклад виклику команди triangle:\n\ttriangle(13, "leg", 25, "opposite angle")');
+console.log("В даному випадку Вам відомо три параметри:\n\t• прямий кут, який одразу відомий і який задавати не потрібно\n\t• катет, який дорівнює 13\n\t• протилежний до катета кут, який дорівнює 25°");
+console.log("");
+console.log("Важливо пам'ятати про правильну сумісність параметрів — якщо Ви задасте два кути, або прилеглий до катета кут і гіпотенузу, то програма не спрацює, оскільки в першому випадку відомі тільки кути, а цього не достатньо для розв'язання трикутника. В другому випадку «прилеглий до катета кут» так і 'кричить' про важливість наявності катета, як другого параметра. Хача було б круто, якби можна було розв'язати трикутник, маючи дві гіпотенузи :) \nАле так не працює, тому будьте уважні при передачі параметрів.");
+
+console.log("()_()\n(•.•)\n/ >♥");
+
+
+// triangle(13, "leg", 35, "hypotenuse");
+// triangle(13, "leg", 25, "opposite angle");
+// triangle(18, "leg", 21, "adjacent angle");
+// // triangle(21, "adjacent angle", 18, "leg");
+// triangle(5, "hypotenuse", 0, "angle");
+// triangle(121, "angle", 13, "hypotenuse");
+// triangle(84, "leg", 14, "leg");
+
