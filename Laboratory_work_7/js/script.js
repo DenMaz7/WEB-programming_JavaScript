4da88b27685922e9cda241750040290c070862d0
DenMaz7 <91177753+DenMaz7@users.noreply.github.com>
2024-05-13 20:36:20 +0200

    update

--- Laboratory_work_7/js/script.js
+++ Laboratory_work_7/js/script.js
@@ -11,6 +11,11 @@
     const bh = {};
 
     const homeHtml = "snippets/home-snippets.html";
+    const allCategoriesUrl = "data/categories.json";
+    //const categoriesTitleHtml = "snippets/home-snippets.html";
+    const categoryHtml = "snippets/category-snippets.html";
+
+
 
     const insertHtml = function (selector, html) {
         const targetElem = document.querySelector(selector);
@@ -21,6 +26,12 @@
         let html = "<div class='text-center'>";
         html += "<img src='../images/ajax-loader.gif'></div>";
         insertHtml(selector, html);
+    }
+
+    const insertProoerty = function (string, propName, propValue) {
+        const propToReplace = "{{" + propName + "}}";
+        string = string.replace(new RegExp(propToReplace, "g", propValue));
+        return string;
     }
 
     document.addEventListener("DOMContentLoaded", function(event) {
@@ -35,14 +46,45 @@
             false
         );
 
-
-
-
-
-
     });
 
 
+    bh.loadCatalogCategories = function () {
+        showLoading("#main");
+
+        $ajaxifyJS.sendGetRequest(
+            allCategoriesUrl,
+            buildAndShowCategoriesHTML);
+    };
+
+
+    function buildAndShowCategoriesHTML (categories) {
+        $ajaxifyJS.sendGetRequest(
+            categoryHtml,
+            function (categoryHtml) {
+                const categoriesViewHtml = buildCategoriesViewHtml(categories, categoryHtml);
+                insertHtml("#main", categoriesViewHtml);
+            },
+            false);
+    }
+
+
+    function buildCategoriesViewHtml (categories, categoryHtml) {
+        
+        let finalHtml = "<div class='catalog'>";
+
+        for (let i = 0; i < categories.length; i++) {
+            let html = categoryHtml;
+            const full_name = "" + categories[i].full_name;
+            const short_name = categories[i].short_name;
+            html = insertHtml(html, full_name);
+            html = insertHtml(html, short_name);
+            finalHtml += html;
+        }
+
+        finalHtml += "</div>";
+        return finalHtml;
+    }
 
     global.$bh = bh;
 
