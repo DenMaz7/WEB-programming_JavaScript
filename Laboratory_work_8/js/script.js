feffefc49816af2fbbd960113c4b349c5f372ddf
DenMaz7 <91177753+DenMaz7@users.noreply.github.com>
2024-06-04 22:51:59 +0200

    update

--- Laboratory_work_8/js/script.js
+++ Laboratory_work_8/js/script.js
@@ -1,50 +1,9 @@
-// document.addEventListener("DOMContentLoaded", 
-//     function(event) {
-        
-//     }
-// );
-
-
-
-(function (global){
-
-    const bh = {};
-
-    const homeHtml = "snippets/home-snippets.html";
-
-    const insertHtml = function (selector, html) {
-        const targetElem = document.querySelector(selector);
-        targetElem.innerHTML = html;
+document.addEventListener("DOMContentLoaded",
+    function(event) {
+        function toggleMenu() {
+            var menu = document.querySelector('ul.menu.large');
+            menu.classList.toggle('show');
+        }
     }
-
-    const showLoading = function (selector) {
-        let html = "<div class='text-center'>";
-        html += "<img src='../images/ajax-loader.gif'</div>";
-        insertHtml(selector, html);
-    }
-
-    document.addEventListener("DOMContentLoaded", function(event) {
-        
-        showLoading("#main");
-
-        $ajaxifyJS.sendGetRequest(
-            homeHtml,
-            function (responseText) {
-                document.querySelector("#main").innerHTML = responseText;
-            },
-            false
-        );
-
-
-
-
-
-
-    });
-
-
-
-    global.$bh = bh;
-
-})(window)
+);
 
