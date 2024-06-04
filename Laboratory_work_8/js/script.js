42b504341290123f37a28ce235b2bf1f50eac888
DenMaz7 <91177753+DenMaz7@users.noreply.github.com>
2024-06-04 23:00:18 +0200

    update

--- Laboratory_work_8/js/script.js
+++ Laboratory_work_8/js/script.js
@@ -1,5 +1,35 @@
 document.addEventListener("DOMContentLoaded",
     function(event) {
+        var currentSlide = 0;
+        var slides = document.querySelectorAll('.slide');
+        var indicators = document.querySelectorAll('.indicator');
+        
+        function showSlide(n) {
+            slides.forEach(function(slide) {
+                slide.classList.remove('active');
+            });
+            indicators.forEach(function(indicator) {
+                indicator.classList.remove('active');
+            });
+            slides[n].classList.add('active');
+            indicators[n].classList.add('active');
+        }
+        
+        function prevSlide() {
+            currentSlide = (currentSlide === 0) ? slides.length - 1 : currentSlide - 1;
+            showSlide(currentSlide);
+        }
+        
+        function nextSlide() {
+            currentSlide = (currentSlide === slides.length - 1) ? 0 : currentSlide + 1;
+            showSlide(currentSlide);
+        }
+        
+        function changeSlide(n) {
+            currentSlide = n;
+            showSlide(currentSlide);
+        }
+        
         function toggleMenu() {
             var menu = document.querySelector('ul.menu.large');
             menu.classList.toggle('show');
